c659e67c5fc160f79f0bdf4cce0cc523346e2ed8
bilal <bilal@macbook-pro.home>
2022-05-21 11:36:00 +0100

    appllo error fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,122 +14,122 @@ import AboutSection from '../components/sections/About'
 import Project from '../components/sections/Project'
 import { repoData } from '../../interface/index'
 
-// type props = {
-//   feature: []
-//   about: {
-//     bio: object[]
-//     skilss: object[]
-//   }
-// }
-const home: NextPage = () => {
+type props = {
+  feature: []
+  about: {
+    bio: object[]
+    skilss: object[]
+  }
+}
+const home: NextPage<props> = ({ feature, about }: props) => {
   return (
     <Fragment>
       <HeroSection />
-      {/* <AboutSection bio={about.bio} skilss={about.skilss} />
-      <Project items={feature} /> */}
+      <AboutSection bio={about.bio} skilss={about.skilss} />
+      <Project items={feature} />
     </Fragment>
   )
 }
 
-// export const getStaticProps: GetStaticProps = async () => {
-//   const httpLink = createHttpLink({
-//     uri: 'https://api.github.com/graphql',
-//   })
+export const getStaticProps: GetStaticProps = async () => {
+  const httpLink = createHttpLink({
+    uri: 'https://api.github.com/graphql',
+  })
 
-//   const authLink = setContext((_, { headers }) => {
-//     return {
-//       headers: {
-//         ...headers,
-//         authorization: `Bearer ${process.env.GITHUB_ACESS_TOKEN}`,
-//       },
-//     }
-//   })
+  const authLink = setContext((_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        authorization: `Bearer ${process.env.GITHUB_ACESS_TOKEN}`,
+      },
+    }
+  })
 
-//   const client = new ApolloClient({
-//     link: authLink.concat(httpLink),
-//     cache: new InMemoryCache(),
-//   })
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  })
 
-//   const { data } = await client.query({
-//     query: gql`
-//       {
-//         user(login: "bilaal441") {
-//           pinnableItems(first: 10) {
-//             nodes {
-//               ... on Repository {
-//                 id
-//                 name
-//                 description
-//                 openGraphImageUrl
-//                 url
-//                 object(expression: "HEAD:README.md") {
-//                   ... on Blob {
-//                     text
-//                   }
-//                 }
-//                 homepageUrl
-//                 repositoryTopics(first: 10) {
-//                   nodes {
-//                     topic {
-//                       name
-//                     }
-//                   }
-//                 }
-//               }
-//             }
-//             edges {
-//               node
-//             }
-//           }
-//         }
-//       }
-//     `,
-//   })
+  const { data } = await client.query({
+    query: gql`
+      {
+        user(login: "bilaal441") {
+          pinnableItems(first: 10) {
+            nodes {
+              ... on Repository {
+                id
+                name
+                description
+                openGraphImageUrl
+                url
+                object(expression: "HEAD:README.md") {
+                  ... on Blob {
+                    text
+                  }
+                }
+                homepageUrl
+                repositoryTopics(first: 10) {
+                  nodes {
+                    topic {
+                      name
+                    }
+                  }
+                }
+              }
+            }
+            edges {
+              node
+            }
+          }
+        }
+      }
+    `,
+  })
 
-//   const { user } = data
+  const { user } = data
 
-//   const pinnableItems = user.pinnableItems.nodes.flat()
+  const pinnableItems = user?.pinnableItems?.nodes?.flat()
 
-//   const items = pinnableItems
-//     .filter(({ homepageUrl }: { homepageUrl: '' }) =>
-//       homepageUrl?.includes('netlify.app'),
-//     )
-//     .map(
-//       ({
-//         id,
-//         name,
-//         description,
-//         homepageUrl,
-//         openGraphImageUrl,
-//         object,
-//         repositoryTopics: { nodes },
-//         url,
-//       }: repoData) => ({
-//         id,
-//         name,
-//         description,
-//         homepageUrl,
-//         imageUrl: openGraphImageUrl,
-//         text: object && object?.text,
-//         topics: nodes?.map(({ topic }: { topic?: { name?: string } }) => {
-//           return topic?.name
-//         }),
-//         url,
-//       }),
-//     )
+  const items = pinnableItems
+    .filter(({ homepageUrl }: { homepageUrl: '' }) =>
+      homepageUrl?.includes('netlify.app'),
+    )
+    .map(
+      ({
+        id,
+        name,
+        description,
+        homepageUrl,
+        openGraphImageUrl,
+        object,
+        repositoryTopics: { nodes },
+        url,
+      }: repoData) => ({
+        id,
+        name,
+        description,
+        homepageUrl,
+        imageUrl: openGraphImageUrl,
+        text: object && object?.text,
+        topics: nodes?.map(({ topic }: { topic?: { name?: string } }) => {
+          return topic?.name
+        }),
+        url,
+      }),
+    )
 
-//   const getBio = await fetch(
-//     `https://bilall-c0e63-default-rtdb.europe-west1.firebasedatabase.app/data.json`,
-//   )
+  const getBio = await fetch(
+    `https://bilall-c0e63-default-rtdb.europe-west1.firebasedatabase.app/data.json`,
+  )
 
-//   const bioData = await getBio.json()
+  const bioData = await getBio.json()
 
-//   return {
-//     props: {
-//       feature: items,
-//       about: bioData,
-//     },
-//   }
-// }
+  return {
+    props: {
+      feature: items ?? [],
+      about: bioData ?? {},
+    },
+  }
+}
 
 export default home
