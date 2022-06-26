287aece7a5f4bc4a086ca39b44ec085b90e506ca
bilal <bilal@macbook-pro.home>
2022-06-26 18:16:54 +0100

    fabricon added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
-// import "../styles/globals.scss";
-
 import Layout from "../components/layout/Layout"
 import type { AppProps } from "next/app"
 import UiContextProvider from "../store/isActiveContext"
 import { IconContext } from "react-icons"
+import Head from "next/head"
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UiContextProvider>
@@ -14,9 +13,38 @@ function MyApp({ Component, pageProps }: AppProps) {
           className: "global-class-name",
         }}
       >
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <>
+          <Head>
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/apple-touch-icon.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/favicon-32x32.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/favicon-16x16.png"
+            />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link
+              rel="mask-icon"
+              href="/safari-pinned-tab.svg"
+              color="#5bbad5"
+            />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta name="theme-color" content="#ffffff" />
+          </Head>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </>
       </IconContext.Provider>
     </UiContextProvider>
   )
